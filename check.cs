74040cb607765455876f962c3f4dc976dd71ee8a
Tim <tmmtsmith@gmail.com>
2013-08-19 07:55:22 -0500

    Added SQL Info

--- check.cs
+++ check.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using FileChecker;
+using System.Data.SqlClient;
 
 namespace FileChecker
 {
@@ -13,14 +13,20 @@ namespace FileChecker
         static void Main(string[] args)
         {
             int x = 0;
-
             while (x < 1)
             {
-                string fileP = @"E:\Check\*.csv";
+                string fileP = @"E:\Check\mo.csv";
                 if (File.Exists(fileP))
                 {
                     x = 1;
-                    Console.WriteLine("File found!  Stopping process");
+                    Console.WriteLine("File found!  Now inserting the data");
+                    using (var scon = Utilities.Connect())
+                    {
+                        SqlCommand bi = new SqlCommand("EXECUTE stp_InsertMOData");
+                        bi.ExecuteNonQuery();
+                        scon.Close();
+                    }
+                    Console.WriteLine("Data inserted.");
                 }
                 else
                 {
@@ -28,8 +34,19 @@ namespace FileChecker
                     Console.WriteLine("Continuing to scan ...");
                 }
             }
-
             Console.ReadLine();
+        }
+
+        public static class Utilities
+        {
+            public static SqlConnection Connect()
+            {
+                SqlConnection scon = new SqlConnection();
+                scon.ConnectionString = "integrated security=SSPI;data source=SERVER;persist security info=False;initial catalog=DATABASE";
+                scon.Open();
+                return scon;
+            }
         }
     }
 }
+
