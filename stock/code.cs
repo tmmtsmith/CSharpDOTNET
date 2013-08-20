638abe0e83b91b8ea6d5d118e1d04761581a6f9e
Tim <tmmtsmith@gmail.com>
2013-08-20 06:40:49 -0500

    Initial csharp code

---
+++ stock/code.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace FileChecker
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 1)
+            {
+                string fileP = @"E:\Check\file.csv";
+                if (File.Exists(fileP))
+                {
+                    x = 1;
+                    Console.WriteLine("File found!  Now inserting the data");
+                    using (var scon = Utilities.Connect())
+                    {
+                        SqlCommand bi = new SqlCommand("EXECUTE stp_InsertMOData");
+                        bi.ExecuteNonQuery();
+                        scon.Close();
+                    }
+                    Console.WriteLine("Data inserted.");
+                }
+                else
+                {
+                    x = 0;
+                    Console.WriteLine("Continuing to scan ...");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public static class Utilities
+        {
+            public static SqlConnection Connect()
+            {
+                SqlConnection scon = new SqlConnection();
+                scon.ConnectionString = "integrated security=SSPI;data source=SERVER;persist security info=False;initial catalog=DATABASE";
+                scon.Open();
+                return scon;
+            }
+        }
+    }
+}
