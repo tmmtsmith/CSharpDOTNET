6f3df98900494bfbe843ea393adf17ffe5b4fb72
Tim <tmmtsmith@gmail.com>
2013-09-15 06:55:14 -0500

    Download data

---
+++ StockAnalysis/down.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            DateTime tod = DateTime.Now;
+            int tDay = tod.Day;
+            int tMonth = tod.Month;
+            tMonth = tMonth - 1;
+
+            Console.WriteLine("Enter a symbol");
+            string userinput = Console.ReadLine();
+            string sym = userinput.ToUpper();
+
+            if (sym.Length > 0)
+            {
+                x = 1;
+            }
+
+            if (x == 1)
+            {
+                DATAPATH;
+            }
+        }
+    }
+}
