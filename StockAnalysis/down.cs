6a9a912cf33fa89f05f252ac830d0329732b57b2
Tim <tmmtsmith@gmail.com>
2013-09-17 04:59:04 -0500

    Update down.cs

--- StockAnalysis/down.cs
+++ StockAnalysis/down.cs
@@ -9,15 +9,38 @@ namespace DSt
     {
         static void Main(string[] args)
         {
-            int x = 0;
-            DateTime tod = DateTime.Now;
-            int tDay = tod.Day;
-            int tMonth = tod.Month;
-            tMonth = tMonth - 1;
-
+            
             Console.WriteLine("Enter a symbol");
-            string userinput = Console.ReadLine();
-            string sym = userinput.ToUpper();
+                string userinput = Console.ReadLine();
+                string sym = userinput.ToUpper();
+
+                Console.WriteLine("Would you like the default historic date?");
+                string answer = Console.ReadLine();
+                answer = answer.ToLower();
+                int pYear, pMonth, pDay;
+
+
+                if (answer == "no")
+                {
+                    Console.WriteLine("Enter historic year");
+                    string sYear = Console.ReadLine();
+                    pYear = Convert.ToInt16(sYear);
+
+                    Console.WriteLine("Enter historic month");
+                    string sMonth = Console.ReadLine();
+                    pMonth = Convert.ToInt16(sMonth);
+                    pMonth = pMonth - 1;
+
+                    Console.WriteLine("Enter historic day");
+                    string sDay = Console.ReadLine();
+                    pDay = Convert.ToInt16(sDay);
+                }
+                else
+                {
+                    pYear = 1993;
+                    pMonth = 1;
+                    pDay = 1;
+                }
 
             if (sym.Length > 0)
             {
