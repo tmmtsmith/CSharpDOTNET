183aeec6506a8b991e7fa5e49bc6607d9a1cbcb3
Tim <tmmtsmith@gmail.com>
2013-09-27 06:29:51 -0500

    Exchanges 1

---
+++ stock/exchange.cs
@@ -0,0 +1,18 @@
+// initial
+
+public static bool FoEx(string exch)
+{
+
+  string forex = ".";
+  bool check = exch.Contains(Ger);
+  return check;
+
+}
+
+// in prog
+
+bool status = Class.FoEx(sym);
+if (status)
+{
+  // check exchanges
+}
