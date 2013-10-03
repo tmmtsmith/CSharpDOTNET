33e64563b1356c20eb0ea3d5798fb3cd7c1a4a19
Tim <tmmtsmith@gmail.com>
2013-10-03 05:53:04 -0500

    Update retdate.cs

--- StockAnalysis/retdate.cs
+++ StockAnalysis/retdate.cs
@@ -3,7 +3,16 @@
 SqlCommand year = new SqlCommand("SELECT YEAR(MAX(Date)) FROM stock.HistoricalData", conn);
 SqlCommand month = new SqlCommand("SELECT MONTH(MAX(Date)) FROM stock.HistoricalData", conn);
 SqlCommand day = new SqlCommand("SELECT DAY(MAX(Date)) FROM stock.HistoricalData", conn);
+
 string y = Convert.ToString(year.ExecuteScalar());
 string m = Convert.ToString(month.ExecuteScalar());
 string d = Convert.ToString(day.ExecuteScalar());
-Console.WriteLine("Latest year is " + y + " latest month is " + m + " latest day is " + d);
+
+upyear = Convert.ToInt16(y);
+upmonth = Convert.ToInt16(m);
+upmonth = upmonth - 1;
+upday = Convert.ToInt16(d);
+
+// IMP
+
+conn.Close()
