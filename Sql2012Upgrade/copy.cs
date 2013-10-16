5063889697a8f49fae32047158ad01bcb4becb27
Tim <tmmtsmith@gmail.com>
2013-10-16 16:37:02 -0500

    First procedures

---
+++ Sql2012Upgrade/copy.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Smo.Agent;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+
+// Added references (refer to: http://msdn.microsoft.com/en-us/library/ms162153.aspx)
+// Microsoft.SqlServer.Smo.dll 
+// Microsoft.SqlServer.ConnectionInfo.dll 
+// Microsoft.SqlServer.Management.Sdk.Sfc.dll 
+
+
+namespace SqlJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server srv = new Server(@"NDB0825PDV");
+            Database db = srv.Databases["ATCode"];
+
+            Scripter stpsc = new Scripter(srv);
+            stpsc.Options.WithDependencies = true;
+
+            foreach (StoredProcedure stp in db.StoredProcedures)
+            {
+                if (stp.IsSystemObject == false)
+                {
+                    Console.WriteLine("/* Create script for procedure " + stp.Name + " */");
+                    System.Collections.Specialized.StringCollection sc2 = stpsc.Script(new Urn[] { stp.Urn });
+
+                    foreach (string sp in sc2)
+                    {
+                        Console.WriteLine(sp);
+                    }
+
+                    Console.WriteLine("\n");
+
+                }
+            }
+
+        }
+    }
+}
