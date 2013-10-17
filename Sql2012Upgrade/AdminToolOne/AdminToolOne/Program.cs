ec1c3d1078e346e04f9cfa86be76b4082dad644b
Tim <tmmtsmith@gmail.com>
2013-10-17 05:53:24 -0500

    Admin Tool 1

---
+++ Sql2012Upgrade/AdminToolOne/AdminToolOne/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Smo.Agent;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+
+namespace AdminToolOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Server srv = new Server(@"SAMSUNG-PC\SQLEXPRESS");
+            string dbName = "TestDatabase";
+            string delete = "Archive";
+            Database db = srv.Databases[dbName];
+            Database delDB = srv.Databases[delete];
+
+            foreach (Table dTab in delDB.Tables)
+            {
+                using (var scon = Connections.Connect())
+                {
+                    SqlCommand drop = new SqlCommand("DROP TABLE " + dTab, scon);
+                    drop.ExecuteNonQuery();
+                    drop.Dispose();
+                    scon.Close();
+                }
+            }
+
+            Scripter tabScr = new Scripter(srv);
+
+            foreach (Table tb in db.Tables)
+            {
+                if (tb.IsSystemObject == false)
+                {
+                    System.Collections.Specialized.StringCollection sctab = tabScr.Script(new Urn[] { tb.Urn });
+
+                    foreach (string stab in sctab)
+                    {
+                        using (var scon = Connections.Connect())
+                        {
+                            SqlCommand copyTables = new SqlCommand(stab, scon);
+                            copyTables.ExecuteNonQuery();
+                            copyTables.Dispose();
+                            scon.Close();
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    public static class Connections
+    {
+        public static SqlConnection Connect()
+        {
+            string server = "SAMSUNG-PC\\SQLEXPRESS";
+            string database = "Archive";
+            SqlConnection scon = new SqlConnection();
+            scon.ConnectionString = "integrated security=SSPI;data source=" + server + ";persist security info=False;initial catalog=" + database + "";
+            scon.Open();
+            return scon;
+        }
+    }
+}
