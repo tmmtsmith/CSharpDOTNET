0ec91e1b89282369ad27756a0c43fe487cb4b0b4
Tim <tmmtsmith@gmail.com>
2013-11-27 05:59:00 -0600

    Create bitstamp.cs

---
+++ bitcoin/bitstamp.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+
+namespace BitCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 1;
+            while (y = 1)
+            {
+                Console.WriteLine("Waiting ... " + "\n");
+
+                Thread.Sleep(180000); // Should be three minutes
+                
+                using (var webC = new System.Net.WebClient())
+                {
+                    var json = webC.DownloadString("https://www.bitstamp.net/api/ticker/");
+                    string x = json.ToString();
+
+                    using (var scon = Utilities.Connect())
+                    {
+                        SqlCommand bitIn = new SqlCommand("INSERT INTO BitstampJSONData (JSONData) SELECT @p", scon);
+                        bitIn.Parameters.Add(new SqlParameter("@p", x));
+                        bitIn.ExecuteNonQuery();
+                        scon.Close();
+                    }
+
+                    Console.WriteLine("Bitcoin data imported." + "\n");
+                    //y++;
+                }
+            }
+        }
+    }
+
+    public static class Utilities
+    {
+        public static SqlConnection Connect()
+        {
+            SqlConnection scon = new SqlConnection();
+            scon.ConnectionString = "integrated security=SSPI;data source=SERVER;persist security info=False;initial catalog=DATABASE";
+            scon.Open();
+            return scon;
+        }
+    }
+}
