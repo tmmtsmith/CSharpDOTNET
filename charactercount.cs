b178caeff79d0779416a24acd77400c30334ae27
Tim <tmmtsmith@gmail.com>
2014-01-09 14:15:16 -0600

    Count characters per line
    Produces a file with the correct and incorrect amount of counted characters.

---
+++ charactercount.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"C:\Users\Public\FileInfo\file.txt";
+
+            int cnt = 0;
+            string line;
+            int count;
+
+            StreamReader readFile = new StreamReader(file);
+            Console.WriteLine("Reviewing the file ...");
+            while ((line = readFile.ReadLine()) != null)
+            {
+                count = line.Split('|').Length - 1;
+                string data = "Line: " + cnt.ToString() + "; | number: " + count.ToString();
+                if (count != 37)
+                {
+                    using (StreamWriter write = File.AppendText(@"C:\Users\Public\FileInfo\SSISFailures.txt"))
+                    {
+                        write.WriteLine(data);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter write = File.AppendText(@"C:\Users\Public\FileInfo\SSISCorrectColumns.txt"))
+                    {
+                        write.WriteLine(data);
+                    }
+                }
+                count = 0;
+                cnt++;
+            }
+            Console.WriteLine("File reviewed.");
+            Console.ReadLine();            
+        }
+    }
+}
