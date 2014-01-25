a360bf76dda05563f83e599e00d882e1f84f8120
Tim <tmmtsmith@gmail.com>
2014-01-25 09:12:30 -0600

    Update filesize.cs

--- filesize.cs
+++ filesize.cs
@@ -21,8 +21,18 @@ namespace FileCrazy
                 Console.WriteLine("{0}: {1} MB", fi.Name, mb);
             }
             Console.ReadLine();
-            // FileWriteTime
 
         }
     }
 }
+
+/*
+
+FileInfo f = new FileInfo(@"C:\file.txt");
+long mb = (f.Length / 1048576);
+DateTime now = DateTime.Now;
+DateTime last = File.GetLastWriteTime(Convert.ToString(f));
+double mins = (now - last).TotalMinutes;
+
+
+*/
