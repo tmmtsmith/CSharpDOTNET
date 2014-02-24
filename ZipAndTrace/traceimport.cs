96bc2793f114253b48e6fb167bcf4a28b1b6f2bf
Tim <tmmtsmith@gmail.com>
2014-02-24 06:08:41 -0600

    Import multiple trace files

--- ZipAndTrace/traceimport.cs
+++ ZipAndTrace/traceimport.cs
@@ -37,53 +37,52 @@ namespace TraceImport
                 Console.WriteLine("Moving file " + newLoc + " ... ");
                 File.Move(Tr, folderOne + newLoc);
 
-                Console.WriteLine("Inserting trace data into the temp table ...");
-
                 // Temp Table (edit):
                 string temp = "TEMPTABLE";
                 //  Saved Data Table (edit):
                 string main = "MAINTABLE";
 
-                using (var scon = Connections.Connect())
+                try
                 {
-                    scon.Open();
-                    SqlCommand traceInsert = new SqlCommand("INSERT INTO " + temp + " SELECT * FROM ::fn_trace_gettable('" + folderOne + newLoc + "', default)", scon);
-                    traceInsert.ExecuteNonQuery();
-                    scon.Close();
-                    //scon.Dispose();
+                    using (var scon = Connections.Connect())
+                    {
+                        scon.Open();
+                        SqlCommand traceInsert = new SqlCommand("INSERT INTO " + temp + " SELECT * FROM ::fn_trace_gettable('" + folderOne + newLoc + "', default)", scon);
+                        traceInsert.ExecuteNonQuery();
+                        scon.Close();
+                        //scon.Dispose();
+                    }
+    
+                    File.Delete(folderOne + newLoc);
+    
+                    using (var scon = Connections.Connect())
+                    {
+                        scon.Open();
+                        SqlCommand moveData = new SqlCommand("stp_InsertTraceData @t,@m", scon);
+                        moveData.Parameters.AddWithValue("@t", temp);
+                        moveData.Parameters.AddWithValue("@m", main);
+                        moveData.ExecuteNonQuery();
+                        scon.Close();
+                        //scon.Dispose();
+                    }
+    
+                    using (var scon = Connections.Connect())
+                    {
+                        scon.Open();
+                        SqlCommand clean = new SqlCommand("TRUNCATE TABLE " + temp, scon);
+                        clean.ExecuteNonQuery();
+                        scon.Close();
+                        //scon.Dispose();
+                    }
+                }
+                catch
+                {
+                    File.Delete(folderOne + newLoc);
                 }
 
-                Console.WriteLine("Deleting file ... ");
-
-                File.Delete(folderOne + newLoc);
-
-                Console.WriteLine("Inserting trace data into main table ... ");
-
-                using (var scon = Connections.Connect())
-                {
-                    scon.Open();
-                    SqlCommand moveData = new SqlCommand("stp_InsertTraceData @t,@m", scon);
-                    moveData.Parameters.AddWithValue("@t", temp);
-                    moveData.Parameters.AddWithValue("@m", main);
-                    moveData.ExecuteNonQuery();
-                    scon.Close();
-                    //scon.Dispose();
-                }
-
-                Console.WriteLine("Cleaning temp table for next file ... ");
-
-                using (var scon = Connections.Connect())
-                {
-                    scon.Open();
-                    SqlCommand clean = new SqlCommand("TRUNCATE TABLE " + temp, scon);
-                    clean.ExecuteNonQuery();
-                    scon.Close();
-                    //scon.Dispose();
-                }
-
-                Console.WriteLine("Data from trace file added and trace file deleted.");
             }
 
+            Console.WriteLine("Completed.")
             Console.ReadLine();
 
         }
@@ -96,7 +95,7 @@ namespace TraceImport
             string server = "SERVER";
             string database = "DATABASE";
             SqlConnection scon = new SqlConnection();
-            scon.ConnectionString = "integrated security=SSPI;data source=" + server + ";persist security info=False;initial catalog=" + database + ";Connection Timeout=90";
+            scon.ConnectionString = "integrated security=SSPI;data source=" + server + ";persist security info=False;initial catalog=" + database + ";Connection Timeout=0";
             return scon;
         }
     }
