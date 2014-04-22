2360e2f5bd9f32549ffd2e6b0bfbdbca94425492
Tim <tmmtsmith@gmail.com>
2014-04-22 10:50:01 -0500

    Update filesandlines.cs

--- SQLRepeat/filesandlines.cs
+++ SQLRepeat/filesandlines.cs
@@ -23,9 +23,8 @@ public static class ReadFiles
     public static string GetLineByNumber(string file, int lineNo)
     {
         System.IO.StreamReader readfile = new System.IO.StreamReader(file);
-        int cnt = lineNo;
         string line = "";
-        for (int i = 1; i < (cnt + 1); i++ )
+        for (int i = 1; i < (lineNo + 1); i++ )
         {
             line = readfile.ReadLine();
             
