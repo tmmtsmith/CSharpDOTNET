385e181ce6b6be3173f486f41c14665ded87c0a9
Tim <tmmtsmith@gmail.com>
2014-04-23 09:47:33 -0500

    Update filesandlines.cs

--- SQLRepeat/filesandlines.cs
+++ SQLRepeat/filesandlines.cs
@@ -10,9 +10,8 @@ public static class ReadFiles
     public static string SelectFirstLine(string file)
     {
         System.IO.StreamReader readfile = new System.IO.StreamReader(file);
-        int cnt = 1;
         string line = "";
-        for (int i = 1; i < (cnt + 1); i++ )
+        for (int i = 1; i < 2; i++ )
         {
             line = readfile.ReadLine();
         }
@@ -30,14 +29,14 @@ public static class ReadFiles
         return line;
     }
 	
-	public static string GetFileName(string file)
+    public static string GetFileName(string file)
     {
 		string f = file.Substring(file.LastIndexOf("\\") + 1);
 		f = f.Substring(0,f.IndexOf("."));
 		return f;
     }
 	
-	public static string GetFileNameWithExtension(string file)
+    public static string GetFileNameWithExtension(string file)
     {
 		string f = file.Substring(file.LastIndexOf("\\") + 1);
 		return f;
