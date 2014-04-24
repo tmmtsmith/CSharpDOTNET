bf4bac6eb45abcac385f5ade588ee2f51a316f1d
Tim <tmmtsmith@gmail.com>
2014-04-24 17:33:26 -0500

    Update filesandlines.cs

--- SQLRepeat/filesandlines.cs
+++ SQLRepeat/filesandlines.cs
@@ -4,89 +4,136 @@
 
 */
 
-
 public static class ReadFiles
 {
     public static string SelectFirstLine(string file)
     {
         System.IO.StreamReader readfile = new System.IO.StreamReader(file);
         string line = "";
-        for (int i = 1; i < 2; i++ )
+        for (int i = 1; i < 2; i++)
         {
             line = readfile.ReadLine();
         }
-        readfile.Close();
-        readfile.Dispose();
         return line;
     }
+
 
     public static string GetLineByNumber(string file, int lineNo)
     {
         System.IO.StreamReader readfile = new System.IO.StreamReader(file);
         string line = "";
-        for (int i = 1; i < (lineNo + 1); i++ )
+        for (int i = 1; i < (lineNo + 1); i++)
         {
             line = readfile.ReadLine();
         }
-        readfile.Close();
-        readfile.Dispose();
         return line;
     }
-	
+
+
     public static string GetFileName(string file)
     {
-	string f = file.Substring(file.LastIndexOf("\\") + 1);
-	f = f.Substring(0,f.IndexOf("."));
-	return f;
+        string f = file.Substring(file.LastIndexOf("\\") + 1);
+        f = f.Substring(0, f.IndexOf("."));
+        return f;
     }
-	
+
+
     public static string GetFileNameWithExtension(string file)
     {
-	string f = file.Substring(file.LastIndexOf("\\") + 1);
-	return f;
+        string f = file.Substring(file.LastIndexOf("\\") + 1);
+        return f;
     }
-    
+
     public static int CountInvalidLines(string file, int validcount, char ch)
     {
-	System.IO.StreamReader readfile = new System.IO.StreamReader(file);
-	int cnt = 0, total;
-	string line;
-	
-	while ((line = readfile.ReadLine()) != null)
-	{
-	    total = line.Split(ch).Length - 1;
-	    if (total != validcount)
-	    {
-	        cnt++;
-	    }
-	}
-        readfile.Close();
-        readfile.Dispose();	
-	return cnt;
-    }
-    
-    public static int CountValidLines(string file, int validcount, char ch)
-    {
-	System.IO.StreamReader readfile = new System.IO.StreamReader(file);
-	int cnt = 0, total;
-	string line;
-	
-	while ((line = readfile.ReadLine()) != null)
-	{
-	    total = line.Split(ch).Length - 1;
-	    if (total == validcount)
-	    {
-	        cnt++;
-	    }
-	}
+        System.IO.StreamReader readfile = new System.IO.StreamReader(file);
+        int cnt = 0, total;
+        string line;
+
+        while ((line = readfile.ReadLine()) != null)
+        {
+            total = line.Split(ch).Length - 1;
+            if (total != validcount)
+            {
+                cnt++;
+            }
+        }
         readfile.Close();
         readfile.Dispose();
-	return cnt;
+        return cnt;
     }
-    
+
+    public static int CountValidLines(string file, int validcount, char ch)
+    {
+        System.IO.StreamReader readfile = new System.IO.StreamReader(file);
+        int cnt = 0, total;
+        string line;
+
+        while ((line = readfile.ReadLine()) != null)
+        {
+            total = line.Split(ch).Length - 1;
+            if (total == validcount)
+            {
+                cnt++;
+            }
+        }
+        readfile.Close();
+        readfile.Dispose();
+        return cnt;
+    }
+
     public static double InvalidToValid(string file, int validcount, char ch)
     {
         double x = Convert.ToDouble((CountInvalidLines(file, validcount, ch))) / Convert.ToDouble((CountValidLines(file, validcount, ch)));
         return x;
+    }
+
+    public static int SaveGoodBadLines(string file, int validcount, char ch)
+    {
+        string loc = file.Substring(0, file.LastIndexOf("\\") + 1);
+        string f = file.Substring(file.LastIndexOf("\\") + 1);
+        f = f.Substring(0, f.IndexOf("."));
+
+        string validfile = loc + f + "_valid.txt";
+        string invalidfile = loc + f + "_invalid.txt";
+
+        if (System.IO.File.Exists(validfile) == true)
+        {
+            System.IO.File.Delete(validfile);
+            System.IO.File.Create(validfile);
+        }
+
+        if (System.IO.File.Exists(invalidfile) == true)
+        {
+            System.IO.File.Delete(invalidfile);
+            System.IO.File.Create(invalidfile);
+        }
+
+
+        System.IO.StreamReader readfile = new System.IO.StreamReader(file);
+        System.IO.StreamWriter writevalid = new System.IO.StreamWriter(validfile);
+        System.IO.StreamWriter writeinvalid = new System.IO.StreamWriter(invalidfile);
+        int cnt = 0, total;
+        string line;
+
+        while ((line = readfile.ReadLine()) != null)
+        {
+            total = line.Split(ch).Length - 1;
+            if (total == validcount)
+            {
+                writevalid.WriteLine(line);
+            }
+            else
+            {
+                writeinvalid.WriteLine(line);
+            }
+        }
+        readfile.Close();
+        readfile.Dispose();
+        writevalid.Close();
+        writevalid.Dispose();
+        writeinvalid.Close();
+        writeinvalid.Dispose();
+        return cnt;
     }
 }
