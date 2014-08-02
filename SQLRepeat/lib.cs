5c2d8b3a211886080c7c6e7bfdde9e566b30546b
Tim <tmmtsmith@gmail.com>
2014-08-02 05:05:16 -0500

    Create lib.cs

---
+++ SQLRepeat/lib.cs
@@ -0,0 +1,8 @@
+public static class StringLibrary
+{
+    public static string SubToMain(string j)
+    {
+        string p = j.Substring((j.IndexOf("[") + 1), (j.IndexOf("]") - (j.IndexOf("[") + 1)));
+        return p;
+    }
+}
